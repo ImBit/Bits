855ea2d86c0c37273a76b431327153997a4419fe
ImBit <151650938+ImBit@users.noreply.github.com>
2025-06-05 10:45:41 +0100

    :sparkles: Automatically translate TranslatableComponents with Text.

--- src/main/java/xyz/bitsquidd/bits/lib/sendable/text/Text.java
+++ src/main/java/xyz/bitsquidd/bits/lib/sendable/text/Text.java
@@ -6,37 +6,71 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.bitsquidd.bits.lib.sendable.Sendable;
 import xyz.bitsquidd.bits.lib.sendable.text.decorator.ITextDecorator;
+import xyz.bitsquidd.bits.lib.sendable.text.decorator.examples.BlankDecorator;
+import xyz.bitsquidd.bits.lib.sendable.text.decorator.examples.TranslationDecorator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Text implements Sendable {
     protected final @NotNull Component component;
-    protected final @Nullable ITextDecorator decorator;
+    protected final @NotNull List<ITextDecorator> decorators = new ArrayList<>();
 
-    public Text(@NotNull Component component, @Nullable ITextDecorator decorator) {
+    private static final List<ITextDecorator> PRE_DEFAULT_DECORATORS = List.of(
+            // These will always be applied first.
+            new TranslationDecorator()
+    );
+    private static final List<ITextDecorator> POST_DEFAULT_DECORATORS = List.of(
+            // These will always be applied last.
+            new BlankDecorator()
+    );
+
+    public Text(@NotNull Component component, @NotNull List<ITextDecorator> decorators) {
         this.component = component;
-        this.decorator = decorator;
+        this.decorators.addAll(decorators);
     }
+
+    public Text(@NotNull Component component, @NotNull ITextDecorator decorator) {
+        this(component, List.of(decorator));
+    }
+
     public Text(@NotNull Component component) {
-        this(component, null);
+        this(component, List.of());
     }
-    public Text(@NotNull String text, @Nullable ITextDecorator formatter) {
+
+    @Deprecated(forRemoval = true)
+    public Text(@NotNull String text, @NotNull ITextDecorator formatter) {
         this(Component.text(text), formatter);
     }
+
+    @Deprecated(forRemoval = true)
     public Text(@NotNull String text) {
-        this(Component.text(text), null);
+        this(Component.text(text), List.of());
     }
 
-    public static Text of(@NotNull Component component, @Nullable ITextDecorator formatter) {
-        return new Text(component, formatter);
+
+    public static Text of(@NotNull Component component, @NotNull List<ITextDecorator> decorators) {
+        return new Text(component, decorators);
     }
+
+    public static Text of(@NotNull Component component, @NotNull ITextDecorator decorator) {
+        return new Text(component, decorator);
+    }
+
     public static Text of(@NotNull Component component) {
         return new Text(component);
     }
+
+    @Deprecated(forRemoval = true)
     public static Text of(@NotNull String text, @Nullable ITextDecorator formatter) {
         return new Text(text, formatter);
     }
+    @Deprecated(forRemoval = true)
     public static Text of(@NotNull String text) {
         return new Text(text);
     }
+
+
 
     @Override
     public <T extends CommandSender> void send(@NotNull T target) {
@@ -44,6 +78,16 @@ public class Text implements Sendable {
     }
 
     public <T extends CommandSender> Component getComponent(@Nullable T target) {
-        return decorator != null ? decorator.format(component, target) : component;
+        Component returnComponent = component;
+
+        List<ITextDecorator> componentDecorators = new ArrayList<>(PRE_DEFAULT_DECORATORS);
+        componentDecorators.addAll(decorators);
+        componentDecorators.addAll(POST_DEFAULT_DECORATORS);
+
+        for (ITextDecorator decorator : componentDecorators) {
+            returnComponent = decorator.format(returnComponent, target);
+        }
+
+        return returnComponent;
     }
 }
---
+++ src/main/java/xyz/bitsquidd/bits/lib/sendable/text/decorator/examples/TranslationDecorator.java
@@ -0,0 +1,29 @@
+package xyz.bitsquidd.bits.lib.sendable.text.decorator.examples;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TranslatableComponent;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.translation.GlobalTranslator;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xyz.bitsquidd.bits.lib.component.ComponentHelper;
+
+import java.util.Locale;
+
+public class TranslationDecorator extends TextTagDecorator {
+    @Override
+    public @NotNull Component format(@NotNull Component component, @Nullable CommandSender target) {
+        if (component instanceof TranslatableComponent translatableComponent) {
+            Locale locale = Locale.getDefault();
+            if (target instanceof Player player) {
+                locale = player.locale();
+            }
+
+            component = GlobalTranslator.render(translatableComponent, locale);
+        }
+
+        return component;
+    }
+}
