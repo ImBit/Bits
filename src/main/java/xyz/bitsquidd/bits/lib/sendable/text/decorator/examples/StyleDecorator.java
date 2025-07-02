962807f654113e3e6c47c33d6fdf960e139bd3fc
ImBit <151650938+ImBit@users.noreply.github.com>
2025-07-02 17:53:10 +0100

    :sparkles: Added compatability for StyleFormatter to read styles other than just TextDecorations - e.g. insertions, shadow colours.

--- src/main/java/xyz/bitsquidd/bits/lib/sendable/text/decorator/examples/StyleDecorator.java
+++ src/main/java/xyz/bitsquidd/bits/lib/sendable/text/decorator/examples/StyleDecorator.java
@@ -1,7 +1,5 @@
 package xyz.bitsquidd.bits.lib.sendable.text.decorator.examples;
 
-import net.kyori.adventure.text.format.TextDecoration;
-
 import xyz.bitsquidd.bits.lib.sendable.text.decorator.AbstractTagDecorator;
 import xyz.bitsquidd.bits.lib.sendable.text.decorator.formatters.DynamicColorFormatter;
 import xyz.bitsquidd.bits.lib.sendable.text.decorator.formatters.StyleFormatter;
@@ -9,11 +7,11 @@ import xyz.bitsquidd.bits.lib.sendable.text.decorator.formatters.StyleFormatter;
 public class StyleDecorator extends AbstractTagDecorator {
     public StyleDecorator() {
         super();
-        formatters.put("b", new StyleFormatter(TextDecoration.BOLD));
-        formatters.put("i", new StyleFormatter(TextDecoration.ITALIC));
-        formatters.put("s", new StyleFormatter(TextDecoration.STRIKETHROUGH));
-        formatters.put("u", new StyleFormatter(TextDecoration.UNDERLINED));
-        formatters.put("o", new StyleFormatter(TextDecoration.OBFUSCATED));
+        formatters.put("b", StyleFormatter.bold());
+        formatters.put("i", StyleFormatter.italic());
+        formatters.put("s", StyleFormatter.strikethrough());
+        formatters.put("u", StyleFormatter.underlined());
+        formatters.put("o", StyleFormatter.obfuscated());
         formatters.put("c", new DynamicColorFormatter());
     }
 }
