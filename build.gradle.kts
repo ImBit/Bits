86093e5cc6c6603130f6b511487096a49c68c6cd
ImBit <151650938+ImBit@users.noreply.github.com>
2025-08-31 17:45:52 +0100

    :art: Automatically add version to plugin.yml.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,19 @@ java {
     withJavadocJar()
 }
 
+tasks.register<Copy>("processPluginYml") {
+    val templateFile = file("src/main/resources/plugin.yml.template")
+    val outputFile = file("src/main/resources/plugin.yml")
+    from(templateFile)
+    into(outputFile.parent)
+    rename { "plugin.yml" }
+    expand("version" to project.version)
+}
+
+tasks.processResources {
+    dependsOn("processPluginYml")
+}
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
