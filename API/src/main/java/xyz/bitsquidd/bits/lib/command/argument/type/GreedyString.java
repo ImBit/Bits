a646c223832bb46e5893ddb4f5e0e5377c38449f
ImBit <151650938+ImBit@users.noreply.github.com>
2025-10-20 20:37:29 +0100

    :art: Overrode toString, equals and hashcode for GreedyString.

--- API/src/main/java/xyz/bitsquidd/bits/lib/command/argument/type/GreedyString.java
+++ API/src/main/java/xyz/bitsquidd/bits/lib/command/argument/type/GreedyString.java
@@ -8,4 +8,24 @@ public class GreedyString {
     public GreedyString(@NotNull String value) {
         this.value = value;
     }
+
+    public static @NotNull GreedyString of(@NotNull String value) {
+        return new GreedyString(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof GreedyString other) && other.value.equals(this.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
 }
