08252e5fb502e373cbd489bdc4f1e7d27bc980e9
ImBit <151650938+ImBit@users.noreply.github.com>
2025-10-22 10:51:04 +0100

    :sparkles: Added suggestions to Boolean args.

--- API/src/main/java/xyz/bitsquidd/bits/lib/command/argument/parser/impl/primitive/BooleanArgumentParser.java
+++ API/src/main/java/xyz/bitsquidd/bits/lib/command/argument/parser/impl/primitive/BooleanArgumentParser.java
@@ -2,9 +2,17 @@ package xyz.bitsquidd.bits.lib.command.argument.parser.impl.primitive;
 
 import org.jetbrains.annotations.NotNull;
 
+import xyz.bitsquidd.bits.lib.command.util.BitsCommandContext;
+
+import java.util.List;
+
 public final class BooleanArgumentParser extends PrimitiveArgumentParserNew<@NotNull Boolean> {
     public BooleanArgumentParser() {
         super(Boolean.class, "Boolean");
     }
 
+    @Override
+    public @NotNull List<String> getSuggestions(@NotNull BitsCommandContext ctx) {
+        return List.of("true", "false");
+    }
 }
