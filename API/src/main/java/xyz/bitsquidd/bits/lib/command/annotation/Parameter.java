550968ebcc8f198cd8fdcbc3a6a98b2627f7dcd7
ImBit <151650938+ImBit@users.noreply.github.com>
2025-11-13 11:21:43 +0000

    :sparkles: Added @Parameter for nicely named args.

---
+++ API/src/main/java/xyz/bitsquidd/bits/lib/command/annotation/Parameter.java
@@ -0,0 +1,11 @@
+package xyz.bitsquidd.bits.lib.command.annotation;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Parameter {
+    @NotNull String value();
+}
