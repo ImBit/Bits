ff04bccf4cf427cd01ca986937ad3e9aec0e1d24
ImBit <151650938+ImBit@users.noreply.github.com>
2026-01-04 12:03:50 +0000

    :art: Configure for JitPack releases.

--- API/build.gradle.kts
+++ API/build.gradle.kts
@@ -9,30 +9,3 @@ dependencies {
 tasks.withType<Jar> {
     archiveBaseName.set("Bits-API")
 }
-
-/**
- * Publishing is configured to use the {@link https://github.com/BiomeBattle/} repository for now,
- * this is the main place we use it.
- * Public repo access will be implemented in the future, when we see a need for it! :)
- */
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifactId = "bits"
-        }
-    }
-    repositories {
-        val repoUser = System.getenv("BB_REPO_USERNAME")
-        val repoPass = System.getenv("BB_REPO_PASSWORD")
-        if (repoUser != null && repoPass != null) {
-            maven {
-                url = uri("https://repo.biomebattle.net/repository/biomebattle-repo/")
-                credentials {
-                    username = repoUser
-                    password = repoPass
-                }
-            }
-        }
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,4 @@ rootProject.name = "Bits"
 include("API")
 
 include("Paper")
-include("Paper:Example")
 include("Velocity")
-
-include("Velocity:Example")
