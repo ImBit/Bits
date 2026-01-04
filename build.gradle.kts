ae3025a8bf178dc8234117bb1ac7e5b3f83e3132
ImBit <151650938+ImBit@users.noreply.github.com>
2026-01-04 12:45:49 +0000

    :bug: Fix build issues 2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ subprojects {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(22))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
     withSourcesJar()
     withJavadocJar()
 }
