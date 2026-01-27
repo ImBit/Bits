61c5b4202764128ba3295f494279ab6b30fb667a
ImBit <151650938+ImBit@users.noreply.github.com>
2026-01-27 12:03:24 +0000

    🎨 Added Empty Wrapper.

---
+++ API/src/main/java/xyz/bitsquidd/bits/lib/wrappers/Empty.java
@@ -0,0 +1,13 @@
+package xyz.bitsquidd.bits.lib.wrappers;
+
+/**
+ * A placeholder class. It has no purpose other than being empty.
+ */
+public final class Empty {
+    private Empty() {}
+
+    public static Empty empty() {
+        return new Empty();
+    }
+
+}
