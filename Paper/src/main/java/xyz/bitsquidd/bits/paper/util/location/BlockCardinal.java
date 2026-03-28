05c3b5bdaa15ec7a702205c57240afa8f1a75d83
ImBit <151650938+ImBit@users.noreply.github.com>
2026-03-28 00:23:10 +0000

    🎨 Implementation of Locatable for BlockLoc, BlockPos, BlockCardinal.

--- Paper/src/main/java/xyz/bitsquidd/bits/paper/util/location/BlockCardinal.java
+++ Paper/src/main/java/xyz/bitsquidd/bits/paper/util/location/BlockCardinal.java
@@ -10,6 +10,10 @@ package xyz.bitsquidd.bits.paper.util.location;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.util.Vector;
+
+import java.util.Objects;
 
 /**
  * Represents an integer block position with an associated cardinal direction.
@@ -26,7 +30,65 @@ public record BlockCardinal(
   int y,
   int z,
   Cardinal cardinal
-) {
+) implements Locatable {
+
+    //region Java Methods
+    @Override
+    public String toString() {
+        return x + ", " + y + ", " + z + ", " + cardinal;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof BlockCardinal(int x1, int y1, int z1, Cardinal cardinal1))) return false;
+
+        return this.x == x1
+          && this.y == y1
+          && this.z == z1
+          && this.cardinal == cardinal1;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z, cardinal);
+    }
+    //endregion
+
+
+    //region Convertors
+    @Override
+    public Location asLocation(World world) {
+        YawAndPitch yawAndPitch = YawAndPitch.from(cardinal);
+        return new Location(world, x + 0.5, y + 0.5, z + 0.5, yawAndPitch.yaw, yawAndPitch.pitch);
+    }
+
+    @Override
+    public Block asBlock(World world) {
+        return world.getBlockAt(x, y, z);
+    }
+
+    @Override
+    public Vector asVector() {
+        return new Vector(x, y, z);
+    }
+    //endregion
+
+
+    //region Getters
+
+    @Override
+    public YawAndPitch getDirection() {
+        return YawAndPitch.from(cardinal);
+    }
+
+
+    //endregion
+
+
+    public BlockCardinal add(int x, int y, int z) {
+        return new BlockCardinal(this.x + x, this.y + y, this.z + z, cardinal);
+    }
 
     public BlockCardinal add(BlockCardinal other) {
         return new BlockCardinal(
@@ -35,11 +97,6 @@ public record BlockCardinal(
           this.z + other.z,
           YawAndPitch.from(cardinal).addWrap(YawAndPitch.from(other.cardinal)).toCardinal()
         );
-    }
-
-    public Location toLocation(World world) {
-        YawAndPitch yawAndPitch = YawAndPitch.from(cardinal);
-        return new Location(world, x + 0.5, y + 0.5, z + 0.5, yawAndPitch.yaw, yawAndPitch.pitch);
     }
 
 }
