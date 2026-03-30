1eb92aa2b6f78da9621b6e740dd10c2ebaf1b772
ImBit <151650938+ImBit@users.noreply.github.com>
2026-03-30 14:17:20 +0100

    :truck: Correct fabric server mappings.

--- Minecraft/Fabric/Client/src/client/java/xyz/bitsquidd/bits/config/FabricClientBitsConfig.java
+++ Minecraft/Fabric/Client/src/client/java/xyz/bitsquidd/bits/config/FabricClientBitsConfig.java
@@ -1,9 +1,4 @@
-package xyz.bitsquidd.bits.config;/*
- * This file is part of a Bit libraries package.
- * Licensed under the GNU Lesser General Public License v3.0.
- *
- * Copyright (c) 2023-2026 ImBit
- */
+package xyz.bitsquidd.bits.config;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.kyori.adventure.audience.Audience;
