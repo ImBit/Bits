e62deace71dd0b029a19be1873b76046d265c0ec
ImBit <151650938+ImBit@users.noreply.github.com>
2026-07-10 20:01:15 +0100

    :art: Added Constants util.

---
+++ api/src/main/java/xyz/bitsquidd/bits/util/math/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * This file is part of a Bit libraries package.
+ * Licensed under the GNU Lesser General Public License v3.0.
+ *
+ * Copyright (c) 2023-2026 ImBit
+ */
+
+package xyz.bitsquidd.bits.util.math;
+
+public final class Constants {
+    private Constants() {}
+
+    public static final double EPSILON = 1e-6; // A small value used for floating-point comparisons
+
+
+}
